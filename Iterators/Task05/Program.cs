ef40875f7b8d598d56968d17c8f1b0085eafc0df
Алина Архипова <aaarkhipova_6@edu.hse.ru>
2021-05-12 17:07:27 +0300

    ура?..

--- Iterators/Task05/Program.cs
+++ Iterators/Task05/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Text;
 
 /* На вход подается число N.
  * Нужно создать коллекцию из N элементов последовательного ряда натуральных чисел, возведенных в 10 степень, 
@@ -26,18 +27,26 @@ namespace Task05
 {
     class Program
     {
+        static int counter = 0;
         static void Main(string[] args)
         {
             try
             {
-                MyDigits myDigits = new MyDigits();
-                IEnumerator enumerator = myDigits.MyEnumerator(value);
+                if (int.TryParse(Console.ReadLine(), out int value) && value > 0)
+                {
+                    MyDigits myDigits = new MyDigits();
+                    IEnumerator enumerator = myDigits.MyEnumerator(value);
 
-                IterateThroughEnumeratorWithoutUsingForeach(enumerator);
-                Console.WriteLine();
-                IterateThroughEnumeratorWithoutUsingForeach(enumerator);
-                Console.WriteLine();
-                IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+                    IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+                    Console.WriteLine();
+                    IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+                    Console.WriteLine();
+                    IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
             }
             catch (ArgumentException)
             {
@@ -47,20 +56,64 @@ namespace Task05
             {
                 Console.WriteLine("ooops");
             }
+
+            Console.ReadLine();
         }
 
         static void IterateThroughEnumeratorWithoutUsingForeach(IEnumerator enumerator)
         {
+            counter++;
+
+            StringBuilder str = new StringBuilder();
+            while (enumerator.MoveNext())
+            {
+                str.Append(enumerator.Current + " ");
+            }
+            string output = str.ToString();
+
+            if (counter % 2 == 0)
+            {
+                var numbers = output.Split();
+                Array.Reverse(numbers);
+                output = String.Join(" ", numbers);
+            }
+
+            output = output.Trim();
+            Console.Write(output);
+            enumerator.Reset();
         }
     }
 
     class MyDigits : IEnumerator // НЕ МЕНЯТЬ ЭТУ СТРОКУ
     {
+        int position = 0;
+        int end;
+        public IEnumerator MyEnumerator(int value)
+        {
+            end = value;
+            return this;
+        }
 
         public bool MoveNext()
         {
-           
+            return position++ != end;
         }
 
+        public void Reset()
+        {
+            position = 0;
+        }
+
+        public object Current
+        {
+            get
+            {
+                return Math.Pow(position, 10);
+            }
+        }
+
+        object IEnumerator.Current => Current;
     }
+
 }
+
